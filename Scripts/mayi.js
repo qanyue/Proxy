915c06c9e24c80b5f82806a38f4110798b26ceba
Chiupam Leung <65508083+chiupam@users.noreply.github.com>
2020-11-04 13:06:27 +0800

    Create mayi.js

---
+++ Scripts/mayi.js
@@ -0,0 +1,22 @@
+const $ = new a()
+
+const schemes = "alipay://platformapi/startapp?appId=60000002"
+
+$.notify("支付宝", "", "领取蚂蚁森林能量", schemes)
+
+$done()
+
+function a() {
+    _isQuanX = typeof $task != "undefined"
+    _isLoon = typeof $loon != "undefined"
+    _isSurge = typeof $httpClient != "undefined" && !_isLoon
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+        if (_isLoon) return $persistentStore.read(key)
+    }
+    this.notify = (title, subtitle, message, url) => {
+        if (_isLoon) $notification.post(title, subtitle, message, url)
+        if (_isQuanX) $notify(title, subtitle, message, { "open-url": url })
+        if (_isSurge) $notification.post(title, subtitle, message, { url: url })
+    }
+}
